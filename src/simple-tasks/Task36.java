228910aaacb75d140483adfe982506280fb83179
Akash Kumar <patel.eng00@gmail.com>
2025-05-23 00:46:48 +0530

    Update Task36.java

--- src/simple-tasks/Task36.java
+++ src/simple-tasks/Task36.java
@@ -5,4 +5,19 @@
  * maximum from interval index 3 - 5 is 3
  */
 public class Task36 {
+          public static void main(String[] args) {
+            int[] a = {4, 2, 5, 5, 1, 3, 2};
+            int max = findMax(a, 3, 5);
+            System.out.println(max);
+        }
+
+        private static int findMax(int[] a, int startIndex, int endIndex) {
+            int max = Integer.MIN_VALUE;
+            for (int i = startIndex; i <= endIndex; i++) {
+                if (a[i] > max) {
+                    max = a[i];
+                }
+            }
+            return max;
+        }
 }
